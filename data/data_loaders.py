b5f89c4336d1b4f80ea1892427e67fdf03adcabb
Martin Dengis <103104881+martinDengis@users.noreply.github.com>
2025-04-25 20:25:46 +0200

    Refactor get_data_loaders to include transforms for dataset creation

--- data/data_loaders.py
+++ data/data_loaders.py
@@ -2,6 +2,7 @@ from torch.utils.data import DataLoader
 from pathlib import Path
 from data.bibnet_dataset import BibNetDataset
 from data.collate_batch import collate_fn
+from data.transform.build import build_train_transforms, build_test_transforms
 
 
 def build_dataset(data_dir, mode, force_reload=False, transform=None):
@@ -33,17 +34,24 @@ def get_data_loaders(data_dir, batch_size):
     """Create data loaders for training, validation, and testing datasets."""
     data_dir = Path(data_dir)
 
+    # Define the transforms (need to be defined here for DataLoader)
+    train_transform = build_train_transforms(data_dir=data_dir)
+    test_transform = build_test_transforms(data_dir=data_dir)
+
     # Create or get datasets
-    train_dataset = build_dataset(data_dir, mode="train")
-    val_dataset = build_dataset(data_dir, mode="valid")
-    test_dataset = build_dataset(data_dir, mode="test")
+    train_dataset = build_dataset(
+        data_dir, mode="train", transform=train_transform)
+    val_dataset = build_dataset(
+        data_dir, mode="valid", transform=test_transform)
+    test_dataset = build_dataset(
+        data_dir, mode="test", transform=test_transform)
 
     # Create data loaders with custom collate function
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                             collate_fn=collate_fn)
+                              collate_fn=collate_fn)
     val_loader = DataLoader(val_dataset, batch_size=batch_size,
-                           collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size,
                             collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size,
+                             collate_fn=collate_fn)
 
     return train_loader, val_loader, test_loader
