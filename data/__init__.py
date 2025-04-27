91d51e90fe22a492998637bfba4039e61c12ced0
martinDengis <martin.dengis@gmail.com>
2025-04-27 14:51:44 +0200

    Remove prepare_dataset from exports and add new prepare_dataset.py for dataset preparation

--- data/__init__.py
+++ data/__init__.py
@@ -1,13 +1,11 @@
 from .data_loaders import get_data_loaders
 from .bibnet_dataset import BibNetDataset
-from .prepare_dataset import main as prepare_dataset
 from .collate_batch import collate_fn, collate_fn_stats
 
 
 __all__ = [
     "get_data_loaders",
     "BibNetDataset",
-    "prepare_dataset",
     "collate_fn",
     "collate_fn_stats",
 ]
