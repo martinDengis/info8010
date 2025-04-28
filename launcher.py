59a4b1c24610e2a8d43df248a8e19bb1630e5ab8
martinDengis <martin.dengis@gmail.com>
2025-04-28 13:18:32 +0200

    Uncomment argument parsing in launcher for model type specification

--- launcher.py
+++ launcher.py
@@ -3,17 +3,17 @@ from utils.wandb_integration import main as launcher
 
 if __name__ == "__main__":
     # ---------- Launcher ----------
-    # parser = argparse.ArgumentParser(description='Launch training with specified model type')
-    # parser.add_argument('--model_type', type=str, default=None,
-    #                    help='Model type to use: bibnet or bibc3net')
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Launch training with specified model type')
+    parser.add_argument('--model_type', type=str, default=None,
+                       help='Model type to use: bibnet or bibc3net')
+    args = parser.parse_args()
 
-    # # Pass model_type to launcher
-    # launcher(model_type=args.model_type)
+    # Pass model_type to launcher
+    launcher(model_type=args.model_type)
 
     # ---------- Local Testing ----------
-    from tools.train_net import train
-    from config.defaults import get_cfg_defaults
-    cfg = get_cfg_defaults()
-    print('Launching training with default config...')
-    train(cfg)
+    # from tools.train_net import train
+    # from config.defaults import get_cfg_defaults
+    # cfg = get_cfg_defaults()
+    # print('Launching training with default config...')
+    # train(cfg)
